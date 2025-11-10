fe520b201437db1087b81e82f87e30bd92244914
anthony040190 <anthony040190@yahoo.com>
2025-11-10 14:12:14 -0700

    add creatBrand

--- src/main/java/com/codeandchill/gfi_asset_inventory/controllers/BrandController.java
+++ src/main/java/com/codeandchill/gfi_asset_inventory/controllers/BrandController.java
@@ -1,6 +1,7 @@
 package com.codeandchill.gfi_asset_inventory.controllers;
 
 import com.codeandchill.gfi_asset_inventory.dtos.BrandDto;
+import com.codeandchill.gfi_asset_inventory.dtos.RegisterBrandRequest;
 import com.codeandchill.gfi_asset_inventory.entities.Brand;
 import com.codeandchill.gfi_asset_inventory.mappers.BrandMapper;
 import com.codeandchill.gfi_asset_inventory.repositories.BrandRepository;
@@ -36,12 +37,24 @@ public class BrandController {
 
         return ResponseEntity.ok(brandMapper.toDto(brand));
     }
+    // Create Mapper for the fields
+    // Convert to Entity
+    // Save it = brandRepository.save(Entity)
+    // Create DTO version = brandMapper.toDto(Entity)
+    // Create URI = uriComponentsBuilder.path("/brands/{id}").buildAndExpand(brandDto.getId()).toUri();
+    // Return ResponseEntity.created(uri).body(dto);
+    @PostMapping
+    public ResponseEntity<BrandDto> addBrand(@RequestBody RegisterBrandRequest request, UriComponentsBuilder uriComponentsBuilder) {
+        // Have to create another DTO(RegisterBrandRequest) which is consist only the fields that will be saved except PK
+        // Since RegisterBrandRequest is DTO it needs to be converted to Entity because repository is only accepting Entity format
+        var brand = brandMapper.toEntity(request);
+        brandRepository.save(brand);
 
-    public ResponseEntity<BrandDto> addBrand(@RequestBody Brand brand, UriComponentsBuilder uriComponentsBuilder) {
+        // Need to get the DTO format in able to create an uri
+        var brandDto = brandMapper.toDto(brand);
+        var uri = uriComponentsBuilder.path("/brands/{id}").buildAndExpand(brandDto.getId()).toUri();
 
-
-
-        return null;
+        return ResponseEntity.created(uri).body(brandDto);
     }
 
     @DeleteMapping("/{id}")
---
+++ src/main/java/com/codeandchill/gfi_asset_inventory/dtos/RegisterBrandRequest.java
@@ -0,0 +1,10 @@
+package com.codeandchill.gfi_asset_inventory.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterBrandRequest {
+    private String brandName;
+}
