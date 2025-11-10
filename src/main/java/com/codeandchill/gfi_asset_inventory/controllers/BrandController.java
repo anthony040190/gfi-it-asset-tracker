a73e5f353bda8892fa748c596fc831357804adeb
anthony040190 <anthony040190@yahoo.com>
2025-11-10 13:23:00 -0700

    add getBrand and delete brand

--- src/main/java/com/codeandchill/gfi_asset_inventory/controllers/BrandController.java
+++ src/main/java/com/codeandchill/gfi_asset_inventory/controllers/BrandController.java
@@ -1,12 +1,13 @@
 package com.codeandchill.gfi_asset_inventory.controllers;
 
 import com.codeandchill.gfi_asset_inventory.dtos.BrandDto;
+import com.codeandchill.gfi_asset_inventory.entities.Brand;
 import com.codeandchill.gfi_asset_inventory.mappers.BrandMapper;
 import com.codeandchill.gfi_asset_inventory.repositories.BrandRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("/brands")
@@ -17,11 +18,42 @@ public class BrandController {
     private final BrandMapper brandMapper;
 
     @GetMapping
-    public Iterable<BrandDto> getAllUsers() {
+    public Iterable<BrandDto> getAllBrands() {
 
         return brandRepository.findAll()
                 .stream()
                 .map(brandMapper::toDto)
                 .toList();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BrandDto> getBrand(@PathVariable Long id) {
+
+        var brand = brandRepository.findById(id).orElse(null);
+
+        if(brand == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(brandMapper.toDto(brand));
+    }
+
+    public ResponseEntity<BrandDto> addBrand(@RequestBody Brand brand, UriComponentsBuilder uriComponentsBuilder) {
+
+
+
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBrand(@PathVariable Long id) {
+
+        var brand = brandRepository.findById(id).orElse(null);
+
+        if(brand == null)
+            return ResponseEntity.notFound().build();
+
+        brandRepository.delete(brand);
+        return ResponseEntity.noContent().build();
+
     }
 }
