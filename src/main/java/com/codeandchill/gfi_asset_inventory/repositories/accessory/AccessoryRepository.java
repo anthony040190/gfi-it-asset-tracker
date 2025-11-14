265494252195f0ac69e579b51f865e83c3baa4c6
anthony040190 <anthony040190@yahoo.com>
2025-11-14 09:42:11 -0700

    delete accessory and computer folders and classes

--- src/main/java/com/codeandchill/gfi_asset_inventory/repositories/accessory/AccessoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeandchill.gfi_asset_inventory.repositories.accessory;
-
-import com.codeandchill.gfi_asset_inventory.entities.accessory.Accessory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccessoryRepository extends JpaRepository<Accessory, Long> {
-}
--- src/main/java/com/codeandchill/gfi_asset_inventory/repositories/computer/ComputerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeandchill.gfi_asset_inventory.repositories.computer;
-
-import com.codeandchill.gfi_asset_inventory.entities.computer.Computer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComputerRepository extends JpaRepository<Computer, Long> {
-}
