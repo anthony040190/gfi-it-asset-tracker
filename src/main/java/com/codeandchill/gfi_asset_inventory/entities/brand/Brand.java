b991cd2a847c1774f6665ae2497b322fe2ea7f85
anthony040190 <anthony040190@yahoo.com>
2025-11-14 15:53:05 -0700

    remove unused imports

--- src/main/java/com/codeandchill/gfi_asset_inventory/entities/brand/Brand.java
+++ src/main/java/com/codeandchill/gfi_asset_inventory/entities/brand/Brand.java
@@ -1,12 +1,7 @@
 package com.codeandchill.gfi_asset_inventory.entities.brand;
 
-import com.codeandchill.gfi_asset_inventory.entities.accessory.Accessory;
-import com.codeandchill.gfi_asset_inventory.entities.computer.Computer;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
@@ -21,10 +16,4 @@ public class Brand {
 
     @Column(name = "brand", nullable = false, unique = true)
     private String brandName;
-
-    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Computer> computers = new ArrayList<>();
-
-    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Accessory> accessories = new ArrayList<>();
 }
