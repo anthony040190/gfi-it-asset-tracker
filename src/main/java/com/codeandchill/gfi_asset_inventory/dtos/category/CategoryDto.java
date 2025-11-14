8b3bd3bc40c42200cf8a30fc4333ca5624c73208
anthony040190 <anthony040190@yahoo.com>
2025-11-14 16:05:08 -0700

    add Category dto

---
+++ src/main/java/com/codeandchill/gfi_asset_inventory/dtos/category/CategoryDto.java
@@ -0,0 +1,9 @@
+package com.codeandchill.gfi_asset_inventory.dtos.category;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryDto {
+    private Long id;
+    private String categoryName;
+}
